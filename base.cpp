4f9008da3ab44086d26cc9a89ad318a71e1835f9
Rhuan Fortes <rhuanfortes6@gmail.com>
2026-06-16 09:38:39 -0300

    novo código

--- base.cpp
+++ base.cpp
@@ -1,10 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-const int linha = 31;
-const int coluna = 29;
+char mapa[31][29] = {    // Mapa do jogo
 
-char mapa[linha][coluna] = {
     "1111111111111111111111111111",
     "1000000000000110000000000001",
     "1011110111110110111110111101",
@@ -38,16 +36,15 @@ char mapa[linha][coluna] = {
     "1111111111111111111111111111"
 };
 
-float posx = 10.365; // posicao do PacMan
-float posy = 3;
-int direction;
+int posx = 15; // posicao do PacMan
+int posy = 14;
 
 int main() {
     // cria a janela
-    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Minha janela", sf::State::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Minha janela");
 
     // cria um quadrado de tamanho 50 (a parede)
-    sf::RectangleShape quad({34.f, 34.f});
+    sf::RectangleShape quad({50.f, 50.f});
     quad.setFillColor(sf::Color(0, 100, 200));
 
     // sprites do PacMan
@@ -57,12 +54,11 @@ int main() {
         return 0;
     }
     sf::Sprite sprite{texture};
-    sf::Clock clock;
-    sf::Time tempoPassado;
-    sf::Time tempoDePasso = sf::seconds(0.05f);
 
     // executa o programa enquanto a janela está aberta
     while (window.isOpen()) {
+
+        
 
         // verifica todos os eventos que foram acionados na janela desde a última iteração do loop
         while (const std::optional event = window.pollEvent()) {
@@ -72,75 +68,45 @@ int main() {
             else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
                   if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
                       window.close();
-                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Left)
-                     direction=1;
-                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Right)
-                     direction=2;   // right key: move o PacMan para direita
-                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Up)
-                     direction=3;   // up key: move o PacMan para cima
-                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Down)
-                     direction=4;   // down key: move o PacMan para baixo
+                      
+                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Left){
+                  if (mapa[posy][posx - 1] != '1')
+                      posx--;   // left key: move o PacMan para esquerda
+                  }
+                    
+                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Right){
+                    if (mapa[posy][posx + 1] != '1')
+                      posx++;   // right key: move o PacMan para direita
+                  }
+                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Up){
+                    if (mapa[posy - 1][posx] != '1')
+                      posy--;   // up key: move o PacMan para cima
+                  }     
+                  else if (keyPressed->scancode == sf::Keyboard::Scancode::Down){
+                    if (mapa[posy + 1][posx] != '1')
+                      posy++;   // down key: move o PacMan para baixo
+                  }   
+                           
             }
         }
 
         // limpa a janela com a cor preta
         window.clear(sf::Color::Black);
-        tempoPassado += clock.restart();
-        if(tempoPassado>=tempoDePasso){
-            if(direction==1){
-                float proxX = posx - 0.1f; 
-                if(mapa[static_cast<int>(posy)][static_cast<int>(proxX)]!='1' &&
-                   mapa[static_cast<int>(posy+0.9f)][static_cast<int>(proxX)]!='1'){
-                    posx = proxX;
-                }
-            }
-            else if(direction==2){
-                float proxX = posx + 0.1f;
-                if(mapa[static_cast<int>(posy)][static_cast<int>(proxX + 0.9f)]!='1' &&
-                   mapa[static_cast<int>(posy + 0.9f)][static_cast<int>(proxX + 0.9f)]!='1'){
-                posx = proxX;
-                }
-            }
-            else if(direction==3){
-                float proxY = posy - 0.1f;
-                if(mapa[static_cast<int>(proxY)][static_cast<int>(posx)]!='1' &&
-                   mapa[static_cast<int>(proxY)][static_cast<int>(posx+0.9f)]!='1'){
-                posy = proxY;
-                }
-            }
-            else if(direction==4){
-                float proxY = posy + 0.1f;
-                if(mapa[static_cast<int>(proxY + 0.9f)][static_cast<int>(posx)]!='1' &&
-                   mapa[static_cast<int>(proxY + 0.9f)][static_cast<int>(posx+0.9f)]!='1'){
-                posy = proxY;
-                }
-            }
-            tempoPassado = sf::Time::Zero;
-        }
-        
 
         // desenhar tudo aqui...
 
-float primeiraColuna = 484.f; // Centraliza no eixo X
-float tamanhoRetangulo = 34.f;
-
-// desenha paredes
-for(int i = 0; i < linha; i++) {
-    for(int j = 0; j < coluna; j++) {
-        // Ignora o caractere nulo '\0' no final de cada string
-        if (mapa[i][j] == '1') {
-            quad.setPosition({primeiraColuna + (j * tamanhoRetangulo), i * tamanhoRetangulo});
-            window.draw(quad);
-        }
-    }
-}
+        // desenha paredes
+        for(int i=0;i<31;i++)
+            for(int j=0;j<29;j++)
+                if (mapa[i][j]=='1') {
+                    quad.setPosition({j*50.f, i*50.f});
+                    window.draw(quad);
+                }
 
         // desenha PacMan
         sprite.setPosition({posx*50.f,posy*50.f});
-        sprite.setScale({0.7, 0.7});
         window.draw(sprite);
 
-        //ruan boiola
         // termina e desenha o frame corrente
         window.display();
     }
