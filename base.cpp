c8dbd3261b5ac3444e23b9c98a54af16f3d11b7d
Paulo Vitor Martins Silva <sou.paulomsilva@gmail.com>
2026-06-16 10:34:33 -0300

    posicao pac man ajustada

--- base.cpp
+++ base.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 
 char mapa[31][29] = {    // Mapa do jogo
-
     "1111111111111111111111111111",
     "1000000000000110000000000001",
     "1011110111110110111110111101",
@@ -41,11 +40,13 @@ int posy = 14;
 
 int main() {
     // cria a janela
-    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Minha janela");
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Minha janela", sf::State::Fullscreen);
 
     // cria um quadrado de tamanho 50 (a parede)
-    sf::RectangleShape quad({50.f, 50.f});
-    quad.setFillColor(sf::Color(0, 100, 200));
+    sf::RectangleShape quad({34.f, 34.f});
+    quad.setFillColor(sf::Color(0, 150, 255, 100));
+    quad.setOutlineColor(sf::Color(0, 50, 255));
+    quad.setOutlineThickness(-2.f);
 
     // sprites do PacMan
     sf::Texture texture;
@@ -99,12 +100,13 @@ int main() {
         for(int i=0;i<31;i++)
             for(int j=0;j<29;j++)
                 if (mapa[i][j]=='1') {
-                    quad.setPosition({j*50.f, i*50.f});
+                    quad.setPosition({484.f + (j*34.f), 13.f + (i*34.f)});
                     window.draw(quad);
                 }
 
         // desenha PacMan
-        sprite.setPosition({posx*50.f,posy*50.f});
+        sprite.setPosition({484.f + (posx * 34.f), 13.f + (posy * 34.f)});
+        sprite.setScale({34.f/texture.getSize().x, 34.f/texture.getSize().y});
         window.draw(sprite);
 
         // termina e desenha o frame corrente
